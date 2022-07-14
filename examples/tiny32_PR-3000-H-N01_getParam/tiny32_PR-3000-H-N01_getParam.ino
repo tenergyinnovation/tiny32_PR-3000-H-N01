790fd47a21fcfd722c746a230e2869d3f3f52aa2
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-07-14 15:13:31 +0700

    Initial project

---
+++ examples/tiny32_PR-3000-H-N01_getParam/tiny32_PR-3000-H-N01_getParam.ino
@@ -0,0 +1,43 @@
+/***********************************************************************
+ * Project      :     tiny32_PR-3000-H-N01_getParam
+ * Description  :     Get value from PR-3000-H-N01 RS485 Soil Humidity Sensor 
+ *                    (This sensor fixed id=2 and fix baudrate = 4800 can't change)  
+ * Hardware     :     tiny32_v3         
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     14/07/2022
+ * Revision     :     1.0
+ * Rev1.0       :     Origital
+ * website      :     http://www.tenergyinnovation.co.th
+ * Email        :     admin@innovation.co.th
+ * TEL          :     +66 82-380-3299
+ ***********************************************************************/
+#include <Arduino.h>
+#include <tiny32_v3.h>
+
+tiny32_v3 mcu;
+
+float temperature,humidity;
+
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.printf("\r\n**** tiny32_PR-3000-H-N01_getAll ****\r\n");
+    mcu.library_version();
+    mcu.PR3000_H_N01_begin(RXD2,TXD2);
+    mcu.buzzer_beep(2);
+}
+
+
+void loop()
+{
+    temperature = mcu.PR3000_H_N01_tempeature();
+    humidity = mcu.PR3000_H_N01_humidity();
+    
+    Serial.printf("Temperature => %.1f C\r\n",temperature);
+    Serial.printf("Humidity => %.1f%c\r\n",humidity,37);
+  
+ 
+  Serial.println("-------------------------------");
+  vTaskDelay(1000);
+}
